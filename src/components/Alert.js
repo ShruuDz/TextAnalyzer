4ac2fb758d17dd143ff1ea7884c1ea989abfaff1
ShruuDz <darshrutidz@gmail.com>
2023-07-17 19:14:09 +0530

    Few Modifications

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 function Alert(props) {
   return (
-    props.alert && <div className='container-sm px-5 my-4'>
-        <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+    <div style={{height:'40px',marginTop:'10px'}}>
+      {props.alert && 
+      <div className='container-sm px-5'>
+        <div className={`alert alert-${props.alert.type} alert-dismissible fade show text-center`} role="alert">
         {props.alert.msg}
-        
         </div>
+      </div>}
     </div>
   )
 }
